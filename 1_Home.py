844fa77626e66a6e2737488326ca1be83e877faa
anshullaxane <anshullaxane@gmail.com>
2026-04-07 23:20:57 +0530

    Refactor Q&A section and analysis method buttons

--- 1_Home.py
+++ 1_Home.py
@@ -63,7 +63,7 @@ col5, col6 = st.columns(2)
 # Buttons
 with col5:
     if st.button("RAW"):
-        st.switch_page("Assets/2_Raw_analysis.py")
+        st.write("pages/2_Raw_analysis.py")
 with col6:
     if st.button("Fasta"):
         st.switch_page("pages/3_FastA_analysis.py")
